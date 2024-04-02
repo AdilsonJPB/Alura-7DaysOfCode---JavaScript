42f5df19cf5605dfa09aa7fa83c8988a9c308984
Adilson <adilsonjuniorpb@gmail.com>
2024-04-01 21:56:33 -0400

    3/7 completos, salvando valores com LocalStorage

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+// Seleciona os elementos de input e o botão
 const nomeValor = document.querySelector('#name');
 const dataValor = document.querySelector('#date');
+const botao = document.querySelector("button");
+// Array para armazenar os dados das pessoas
+const dadosDaPessoa = [];
 
-nomeValor.addEventListener('input', () => exibirNoConsole(nomeValor.value));
-dataValor.addEventListener('input', () => exibirNoConsole(dataValor.value));
+// Adiciona um ouvinte de evento de clique ao botão
+botao.addEventListener('click', (event)=> {
+    // Previne o comportamento padrão do formulário de ser enviado
+    event.preventDefault();
 
-function exibirNoConsole(valor) {
-    console.log(valor);
-}
+    // Obtém os valores digitados nos campos de nome e data
+    const nome = nomeValor.value;
+    const data = dataValor.value;
+    // Cria um objeto pessoa e o adiciona ao array
+    dadosDaPessoa.push({ nome, data });
+    
+    // Salva o array no localStorage como uma string JSON
+    localStorage.getItem('pessoas', JSON.stringify(dadosDaPessoa));
+    console.log(dadosDaPessoa);
+});
